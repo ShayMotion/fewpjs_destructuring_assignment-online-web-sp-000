5dd4c1edee99b6245aa028559d13fa9a69d5d4ab
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-11 20:51:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ const k_muppet = {
   k_partner: 'Miss Piggy'
 };
 
+const {song_2, song_4} = k_muppet.k_album.the_muppet_movie;
+const {k_job, k_partner} = k_muppet;
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
