a1d76db4b5663544eff548df8f84660d5c37caa3
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-11 20:15:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const farmAnimals = 'cow horse sheep pig chicken'
-const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
   
 }
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
